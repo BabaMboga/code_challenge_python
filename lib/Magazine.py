bfd0f3dd0b1902fd724c820ee1f28863e990d6cf
AyimWilliam <odhisayim@gmail.com>
2023-05-28 15:50:05 +0300

    completed magazine deliverables

--- lib/Magazine.py
+++ lib/Magazine.py
@@ -1,7 +1,10 @@
 class Magazine:
+    magazines_list = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self.magazines_list.append(self)
 
     #name can be changed
     def set_name(self, new_name):
@@ -14,3 +17,8 @@ class Magazine:
     #returns category
     def category(self):
         return self._category
+    
+    #returns a list of all magazine instances
+    @classmethod
+    def all(cls):
+        return cls.magazines_list
