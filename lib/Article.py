f59edea816d5cf8d465f4a6b6371c643d554d7cf
AyimWilliam <odhisayim@gmail.com>
2023-05-28 16:20:37 +0300

    additional updates

--- lib/Article.py
+++ lib/Article.py
@@ -1,2 +1,21 @@
 class Article:
-    pass
+    articles_list = []
+
+    def __init__(self, author, magazine, title):
+        self.author = author
+        self.magazine = magazine
+        self.title = title
+        self.articles_list.append(self)
+
+    #return the title for that given article
+    def title(self):
+        return self.title
+    
+    #returns a list of all article instances
+    @classmethod
+    def all(cls):
+        return cls.articles_list
+    
+    #returns the author for that given article
+    def author(self):
+        return self._author
